9635e941d97d9b36707a1461c6f7a6ecdde9cc0b
pirorino-jp <59248293+pirorino-jp@users.noreply.github.com>
2021-08-08 10:27:46 +0900

    Add files via upload
    Fargateから起動できる様環境変数を設定済み
    DB接続はtry-catch修正済み

--- FastAPI2/db.py
+++ FastAPI2/db.py
@@ -1,20 +1,35 @@
 import databases
 import sqlalchemy
+import os
+
+# DATABASE = 'postgres'
+# AWS
+# USER = os.environ['USER']
+# PASS = os.environ['PASS']
+# PORT = '5432'
+# Database_endpoint = os.environ['ENDPOINT']
+# DB_NAME = os.environ['DB_NAME']
+
+# LOCAL
+USER = 'saru'
+PASS = 'monkey'
+Database_endpoint = 'localhost'
 
 DATABASE = 'postgres'
-USER = 'saru'
-PASSWORD = 'monkey'
-HOST = 'localhost'
 PORT = '5432'
 DB_NAME = 'fastapidb'
 
-DATABASE_URL = '{}://{}:{}@{}:{}/{}'.format(DATABASE, USER, PASSWORD, HOST, PORT, DB_NAME)
+DATABASE_URL = '{}://{}:{}@{}:{}/{}'.format(DATABASE, USER, PASS, Database_endpoint, PORT, DB_NAME)
 
-# databases
-database = databases.Database(DATABASE_URL, min_size=5, max_size=20)
+try:
+    print("Connecting to "+Database_endpoint)
+    print("DATABASE_URL "+DATABASE_URL)
+    # databases
+    database = databases.Database(DATABASE_URL, min_size=5, max_size=20)
+    ECHO_LOG = False
+    engine = sqlalchemy.create_engine(DATABASE_URL, echo=ECHO_LOG)
+    metadata = sqlalchemy.MetaData()
+    print ("Connection successful to "+Database_endpoint)
 
-ECHO_LOG = False
-
-engine = sqlalchemy.create_engine(DATABASE_URL, echo=ECHO_LOG)
-
-metadata = sqlalchemy.MetaData()
+except Exception as e:
+    print ("Connection unsuccessful due to "+str(e))
